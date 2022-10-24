4d1c57319621e347c1de38be7b005bef9783d0ff
Bahodir Abdusaxatov <88647942+Bahodir22@users.noreply.github.com>
2022-10-24 15:50:11 +0500

    1
    ###

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for (int x=1; x<=height; x++){
+            for (int y=1; y<=height; y++){
+                if (y==1 || y==height || x==y || x+y==height+1){
+                    System.out.print("8");
+                }
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        for (int x=1; x<=cathetusLength; x++){
+            for (int y=1; y<=cathetusLength; y++){
+                if (x==1 || y==5 || x==y){
+                    System.out.print("8");
+                }
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
